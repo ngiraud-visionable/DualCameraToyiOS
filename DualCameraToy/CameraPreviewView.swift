3d840523e185fd38e9c49703da04945614886967
Niko Giraud <niamke.giraud@gmail.com>
2022-03-15 15:43:00 -0400

    Configures the toy app

---
+++ DualCameraToy/CameraPreviewView.swift
@@ -0,0 +1,20 @@
+//
+//  CameraPreviewView.swift
+//  Camera Tutorial
+//
+//  Created by Niko Giraud on 3/14/22.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraPreviewView: UIView {
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+    
+    /// Convenience wrapper to get layer as its statically known type.
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+}
